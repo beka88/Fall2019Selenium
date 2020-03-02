e3e711b77ef5b2144c319aee56da7d2a59fc83da
beka88 <bekakadyrova@gmail.com>
2020-03-02 01:36:11 -0600

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -10,7 +10,22 @@ public class BasicNavigation {
         ChromeDriver driver = new ChromeDriver();
         driver.get("http://google.com");
         Thread.sleep(3000);
+
+        String title = driver.getTitle();
+        String expectedTitle ="Google";
+        System.out.println("Title is " +title);
+
+        if(expectedTitle.equals(title)){
+            System.out.println("Test passed");
+        }else{
+            System.out.println("Test failed");
+        }
+
         driver.close();
+
+
+
+
 
 
 
